b987e442dffe6cd2dc208165a82a860bdf5b2024
Methro <root@sandrac-desktop>
2025-02-15 20:26:01 -0500

    Possible Fix

--- src/client/mapblock_mesh.cpp
+++ src/client/mapblock_mesh.cpp
@@ -60,11 +60,7 @@ void MeshMakeData::setCrack(int crack_level, v3s16 crack_pos)
 
 void MeshMakeData::setSmoothLighting(bool smooth_lighting)
 {
-
 	m_smooth_lighting = smooth_lighting && !g_settings->getBool("fullbright");
-
-	if (crack_level >= 0)
-		m_crack_pos_relative = crack_pos - m_blockpos*MAP_BLOCKSIZE;
 }
 
 /*
@@ -79,6 +75,8 @@ static u8 getInteriorLight(enum LightBank bank, MapNode n, s32 increment,
 	const NodeDefManager *ndef)
 {
 
+if (g_settings->getBool("fullbright"))
+	return 255;
 
 	u8 light = n.getLight(bank, ndef->getLightingFlags(n));
 	light = rangelim(light + increment, 0, LIGHT_SUN);
@@ -102,6 +100,8 @@ u16 getInteriorLight(MapNode n, s32 increment, const NodeDefManager *ndef)
 */
 static u8 getFaceLight(enum LightBank bank, MapNode n, MapNode n2, const NodeDefManager *ndef)
 {
+	if (g_settings->getBool("fullbright"))
+		return 255;
 
 	ContentLightingFlags f1 = ndef->getLightingFlags(n);
 	ContentLightingFlags f2 = ndef->getLightingFlags(n2);
